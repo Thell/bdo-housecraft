b64000f7e6d3c37066bfbd332a219e8b0873eb7e
Thell 'Bo' Fowler <thell@tbfowler.net>
2023-03-12 16:39:38 -0500

    gitignore: vscode directory

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
+use anyhow::{Ok, Result};
 use clap::Parser;
 
 pub mod cli_args;
 use cli_args::Cli;
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
     println!("{cli:#?}");
+    Ok(())
 }
